2f9e6708bc5b69a5a6201f5180fddfb47b5c2376
DeZenTID <83010300+DeZenTID@users.noreply.github.com>
2023-12-16 18:51:39 +0700

    Remove one (1) import

--- Project_Akhir_BAD_LAB/src/Application/Main.java
+++ Project_Akhir_BAD_LAB/src/Application/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
