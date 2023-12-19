507fc4d68eb11c428df50043c7e02c1b61e9bab0
Indie Gozal <indie.gozal@binus.ac.id>
2023-12-19 16:46:38 +0700

    Remove unused imports

--- Project_Akhir_BAD_LAB/src/application/Login.java
+++ Project_Akhir_BAD_LAB/src/application/Login.java
@@ -2,17 +2,12 @@ package application;
 
 
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
